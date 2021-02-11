62fb39d3313e1cc10f91411d0f00f17683598045
Mert Aksoy <mert.aksoy@codesquare.at>
2021-02-11 19:58:26 +0300

    Simple face detection using haar cascade frontal face

---
+++ face-detection.py
@@ -0,0 +1,20 @@
+import cv2
+
+faceCascade = cv2.CascadeClassifier("assets/haarcascade_frontalface_default.xml")
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+while True:
+    success, img = cap.read()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow("Camera", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
